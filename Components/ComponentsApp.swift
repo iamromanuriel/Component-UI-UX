f48500aad43699ce34e4ebc059b24ec88cb1071a
iamromanuriel <romanufranciscomartinez@gmail.com>
2025-05-16 12:21:46 -0600

    Initial Commit

---
+++ Components/ComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentsApp.swift
+//  Components
+//
+//  Created by Roman Uriel on 16/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComponentsTests/ComponentsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentsTests.swift
+//  ComponentsTests
+//
+//  Created by Roman Uriel on 16/05/25.
+//
+
+import Testing
+@testable import Components
+
+struct ComponentsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
